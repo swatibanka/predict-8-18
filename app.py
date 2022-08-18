6a2ed3098ca7f72d39e4eb51f76353dfe5b4cf6d
swati banka <83939447+swatibanka@users.noreply.github.com>
2022-08-18 19:10:40 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -92,26 +92,27 @@ model = pickle.load(open("model.pkl", "rb"))
 
 @app.route("/")
 def Home():
-    return jsonify("Hello World")
+    return "Hello World"
 
 @app.route("/predict", methods = ["GET","POST"])
 def predict():
-    x_future_date = pd.date_range(start ="2022-08-01", end = "2023-01-31")
-    x_future_dates = pd.DataFrame()
-    x_future_dates["Dates"] = pd.to_datetime(x_future_date)
-    x_future_dates.index = x_future_dates["Dates"]
-    X, y = create_features(x_future_dates, label='Dates')
-    y_future_total_tickets = model.predict(X)
-    prediction_list = y_future_total_tickets.tolist()
+    return "hi"
+    # x_future_date = pd.date_range(start ="2022-08-01", end = "2023-01-31")
+    # x_future_dates = pd.DataFrame()
+    # x_future_dates["Dates"] = pd.to_datetime(x_future_date)
+    # x_future_dates.index = x_future_dates["Dates"]
+    # X, y = create_features(x_future_dates, label='Dates')
+    # y_future_total_tickets = model.predict(X)
+    # prediction_list = y_future_total_tickets.tolist()
 
-    output = {'data': []}
-    index = 0
-    print(type(y_future_total_tickets.tolist()))
+    # output = {'data': []}
+    # index = 0
+    # print(type(y_future_total_tickets.tolist()))
 
-    for date in x_future_date:
-        output['data'].append({'date': str(date), 'prediction': prediction_list[index]})
-        index = index + 1
-    return json.dumps(output)
+    # for date in x_future_date:
+    #     output['data'].append({'date': str(date), 'prediction': prediction_list[index]})
+    #     index = index + 1
+    # return json.dumps(output)
 
 if __name__ == "__main__":
         app.run()
